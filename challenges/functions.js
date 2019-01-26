00ed408e71a93b2244a065a2847926e624201e71
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-01-26 10:24:22 -0800

    Finished functions.js tasks

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(any1, any2, cb) {
+  return cb(any1, any2);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +18,22 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(any1, any2) {
+  return any1 + any2;
+}
+
+function multiply(any1, any2) {
+  return any1 * any2;
+}
+function greeting(any1, any2) {
+  return `${any1} ${any2}, nice to meet you!`;
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +41,7 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+/* It's possible because functions in JS are also closures, so nestedFunction() body has access to variable internal as well as myFunction() body has the same access to variable external. If some variable isn't defined in a function's scope, the function will look outside (outward!) the scope chain and search for a variable being referenced in the outer scope. */
 
 
 const external = "I'm outside the function";
